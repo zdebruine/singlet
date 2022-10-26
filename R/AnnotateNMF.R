e5f736cfe0b4746e45c02ccf8c92db52d70812e6
zdebruine <zacharydebruine@gmail.com>
2022-10-26 14:11:29 -0400

    adding check for max.levels in AnnotateNMF

--- R/AnnotateNMF.R
+++ R/AnnotateNMF.R
@@ -1,11 +1,12 @@
-#' annotate an NMF model 
-#' 
+#' annotate an NMF model
+#'
 #' @param object    an object suitable for annotation (Seurat, DimReduc, or nmf)
 #' @param columns   factor columns of meta.data (see below) to annotate against
 #' @param meta.data a data.frame, if one is not already part of the object
 #' @param designs   named list of design matrices (supersedes meta.data/columns)
-#' @param center    center the factor matrix for testing? (TRUE) 
-#' @param scale     scale the factor matrix for testing? (FALSE) 
+#' @param center    center the factor matrix for testing? (TRUE)
+#' @param scale     scale the factor matrix for testing? (FALSE)
+#' @param max.levels maximum number of levels a factor may have in order to be included in analysis
 #' @param ... not implemented
 #' @export
 #'
@@ -15,7 +16,7 @@ AnnotateNMF <- function(object, ...) {
 
 
 #' Annotate NMF model with cell or sample metadata
-#' 
+#'
 #' @rdname AnnotateNMF
 #' @aliases AnnotateNMF
 #'
@@ -23,13 +24,11 @@ AnnotateNMF <- function(object, ...) {
 #'
 #' @export
 #'
-AnnotateNMF.DimReduc <- function(object, meta.data=NULL, columns=NULL, designs=NULL, center=TRUE, scale=FALSE, ...){
-
-  designs <- getDesigns(columns=columns, meta.data=meta.data, designs=designs)
-  fits <- lapply(designs, getModelFit, object=object, center=center,scale=scale)
+AnnotateNMF.DimReduc <- function(object, meta.data = NULL, columns = NULL, designs = NULL, center = TRUE, scale = FALSE, max.levels = 200, ...) {
+  designs <- getDesigns(columns = columns, meta.data = meta.data, designs = designs, max.levels)
+  fits <- lapply(designs, getModelFit, object = object, center = center, scale = scale)
   object@misc$annotations <- lapply(fits, getModelResults)
   return(object)
-
 }
 
 
@@ -45,24 +44,26 @@ AnnotateNMF.DimReduc <- function(object, meta.data=NULL, columns=NULL, designs=N
 #'
 #' @param reduction the reductions slot in the Seurat object containing the model to annotate
 #'
-#' @examples 
+#' @examples
 #' \dontrun{
-#' get_pbmc3k_data() %>% NormalizeData %>% RunNMF -> pbmc3k
+#' get_pbmc3k_data() %>%
+#'   NormalizeData() %>%
+#'   RunNMF() -> pbmc3k
 #' AnnotateNMF(pbmc3k)
 #' }
 #' @aliases AnnotateNMF
 #'
 #' @export
-#' 
-AnnotateNMF.Seurat <- function(object, columns = NULL, reduction = "nmf", ...){
-
+#'
+AnnotateNMF.Seurat <- function(object, columns = NULL, reduction = "nmf", ...) {
   if (is.null(columns)) columns <- colnames(object@meta.data)
-  object@reductions[[reduction]] <- 
-    AnnotateNMF.DimReduc(object=object@reductions[[reduction]], 
-                         meta.data=object@meta.data[, columns], 
-                         columns=columns, ...)
+  object@reductions[[reduction]] <-
+    AnnotateNMF.DimReduc(
+      object = object@reductions[[reduction]],
+      meta.data = object@meta.data[, columns],
+      columns = columns, ...
+    )
   return(object)
-
 }
 
 
@@ -75,9 +76,9 @@ AnnotateNMF.Seurat <- function(object, columns = NULL, reduction = "nmf", ...){
 
 
 #' Annotate NMF model with cell metadata
-#' 
+#'
 #' @details Maps factor information in an RcppML::nmf object against meta.data
-#' 
+#'
 #' @rdname AnnotateNMF
 #' @aliases AnnotateNMF
 #'
@@ -85,13 +86,11 @@ AnnotateNMF.Seurat <- function(object, columns = NULL, reduction = "nmf", ...){
 #'
 #' @export
 #'
-AnnotateNMF.nmf <- function(object, meta.data, columns=NULL, designs=NULL, center=TRUE, scale=FALSE, ...){
-
-  designs <- getDesigns(columns=columns,meta.data=meta.data,designs=designs,...)
-  fits <- lapply(designs, getModelFit, object=object, center=center,scale=scale)
+AnnotateNMF.nmf <- function(object, meta.data, columns = NULL, designs = NULL, center = TRUE, scale = FALSE, max.levels = 200, ...) {
+  designs <- getDesigns(columns = columns, meta.data = meta.data, designs = designs, max.levels, ...)
+  fits <- lapply(designs, getModelFit, object = object, center = center, scale = scale)
   object@misc$annotations <- lapply(fits, getModelResults)
   return(object)
-
 }
 
 
