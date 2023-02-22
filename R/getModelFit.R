4a5dfb89dc4ef266351fcf2515b1edf8bef54928
Tim Triche, Jr <tim.triche@gmail.com>
2023-02-22 14:15:55 -0500

    fix transpose in getModelFit

--- R/getModelFit.R
+++ R/getModelFit.R
@@ -25,7 +25,7 @@ getModelFit <- function(design, object, center=TRUE, ...) {
   dat <- object
   if (is(object, "nmf")) dat <- object@h # RcppML nmf 
   if (is(object, "Seurat")) dat <- object@assays$RNA@data
-  if (is(object, "DimReduc")) dat <- t(object@cell.embeddings) # Seurat DimReduc
+  if (is(object, "DimReduc")) dat <- object@cell.embeddings 
   if (is(object, "SingleCellExperiment")) dat <- logcounts(object)
   # SingleCellExperiment::reducedDim(object, dimname) just returns a data.matrix
 
@@ -42,9 +42,15 @@ getModelFit <- function(design, object, center=TRUE, ...) {
       message("Subsetting data to non-NA observations to match design matrix.")
       tofit <- dat[, rownames(design)]
     }
-  } else { 
-    warning("Design matrix has appropriate rank, but no row names. Beware!")
+  } else {
     tofit <- dat
+    if (is.null(rownames(design))) {
+      warning("Design matrix has appropriate rank, but no row names. Beware!")
+    } else if (!identical(rownames(design), colnames(tofit))) {
+      warning("Design matrix row names do not match data observation names!")
+      warning("This is usually a VERY BAD THING. You MUST check your data.")
+      warning("If this warning message persists, file a bug with a reprex.")
+    }
   }
 
   if (center) tofit <- t(scale(t(tofit), ...))
