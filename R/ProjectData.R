bfe065183d88b6e7d5b2200cf698e096cccadeaf
Tim Triche, Jr <tim.triche@gmail.com>
2023-05-20 14:25:10 -0400

    turn off rescaling by default

--- R/ProjectData.R
+++ R/ProjectData.R
@@ -5,11 +5,17 @@
 #' @inheritParams run_nmf
 #' @param w matrix giving the factor model, of dimensions \code{nrow(A) x k}
 #' @return list of \code{h} and \code{d}, where \code{d} gives the relative contribution of each factor in \code{h} to the model
+#'
 #' @export
+#'
 project_model <- function(A, w, L1 = 0.01, L2 = 0, threads = 0) {
-  if (nrow(w) != nrow(A) & ncol(w) != nrow(A)) stop("'w' must share a common edge with the rows of 'A'")
-
-  singlet:::c_project_model(A, w, L1, L2, threads)
+  if (nrow(w) != nrow(A) & 
+      ncol(w) != nrow(A)) {
+    stop("'w' must share a common edge with the rows of 'A'")
+  } else { 
+    # leftover from testing 
+    singlet:::c_project_model(A, w, L1, L2, threads)
+  }
 }
 
 
@@ -19,6 +25,7 @@ project_model <- function(A, w, L1 = 0.01, L2 = 0, threads = 0) {
 #'
 #' @inheritParams RunNMF.Seurat
 #' @param w a factor model of the same number of rows as the assay to be projected in the current object, where columns correspond to factors
+#' @param reorder reorder the factors of the projection by d? (FALSE) 
 #' @param ... not implemented
 #' @details Use \code{set.seed()} to guarantee reproducibility!
 #' @export
@@ -35,6 +42,7 @@ ProjectData.Seurat <- function(object, w,
                           reduction.name = "nmf_projection",
                           reduction.key = "NNLS_",
                           threads = 0,
+                          reorder = FALSE, 
                           ...) {
   if (is.null(assay)) {
     assay <- names(object@assays)[[1]]
@@ -65,10 +73,14 @@ ProjectData.Seurat <- function(object, w,
   rownames(nmf_model$h) <- colnames(nmf_model$w) <- paste0(reduction.key, 1:nrow(nmf_model$h))
   rownames(nmf_model$w) <- rownames(A)
   colnames(nmf_model$h) <- cnames
-  idx <- order(nmf_model$d, decreasing = TRUE)
-  nmf_model$h <- nmf_model$h[idx, ]
-  nmf_model$d <- nmf_model$d[idx]
-  nmf_model$w <- nmf_model$w[, idx]
+
+  if (reorder) { 
+    idx <- order(nmf_model$d, decreasing = TRUE)
+    nmf_model$h <- nmf_model$h[idx, ]
+    nmf_model$d <- nmf_model$d[idx]
+    nmf_model$w <- nmf_model$w[, idx]
+  }
+  
   object@reductions[[reduction.name]] <- new("DimReduc",
                                              cell.embeddings = t(nmf_model$h),
                                              feature.loadings.projected = nmf_model$w,
@@ -85,8 +97,9 @@ ProjectData.Seurat <- function(object, w,
 #'
 #' @inheritParams RunNMF.SingleCellExperiment
 #'
-#' @param w   factor loadings with nrow(w) equal to nrow(object)
-#' @param ... not implemented
+#' @param w       factor loadings with nrow(w) equal to nrow(object)
+#' @param reorder reorder the factors of the projection by d? (FALSE) 
+#' @param ...     not implemented
 #'
 #' @details Use \code{set.seed()} to guarantee reproducibility!
 #'
@@ -110,6 +123,7 @@ ProjectData.SingleCellExperiment <- function(object,
                                              reduction.name = "NMF",
                                              reduction.key = "NMF_",
                                              threads = 0,
+                                             reorder = FALSE,
                                              ...) {
 
   # check if data exists and has been normalized
@@ -131,38 +145,42 @@ ProjectData.SingleCellExperiment <- function(object,
   
   # check if we can proceed 
   stopifnot(assay %in% assayNames(object))
+  if (!all(rownames(x) %in% rownames(A)) | 
+      !all(rownames(A) %in% rownames(w))) {
+    warning("Some genes are missing from the data to be projected.")
+  }
   w <- w[which(rownames(w) %in% rownames(A)), ]
   A <- A[which(rownames(A) %in% rownames(w)), ]
  
   # project
   nmf_model <- project_model(A, w, L1, L2, threads)
   nmf_model$w <- w
-  rownames(nmf_model$h) <- 
-    colnames(nmf_model$w) <- 
-      paste0(reduction.key, 1:nrow(nmf_model$h))
+
+  # label
+  colnames(nmf_model$w) <- paste0(reduction.key, 1:nrow(nmf_model$h))
+  rownames(nmf_model$h) <- colnames(nmf_model$w)
   rownames(nmf_model$w) <- rownames(A)
   colnames(nmf_model$h) <- cnames
-  idx <- order(nmf_model$d, decreasing = TRUE)
-  nmf_model$h <- nmf_model$h[idx, ]
-  nmf_model$d <- nmf_model$d[idx]
-  nmf_model$w <- nmf_model$w[, idx]
+ 
+  # reorder?
+  if (reorder) { 
+    idx <- order(nmf_model$d, decreasing = TRUE)
+    nmf_model$h <- nmf_model$h[idx, ]
+    nmf_model$d <- nmf_model$d[idx]
+    nmf_model$w <- nmf_model$w[, idx]
+  }
   
   # store the results
   lem <- LinearEmbeddingMatrix(sampleFactors = t(nmf_model$h),
                                featureLoadings = nmf_model$w,
                                metadata = list(d = nmf_model$d))
   reducedDim(object, reduction.name) <- lem
+
+  # return
   object
 
 }
 
-
-#' @export
-#' @rdname ProjectData
-#'
-ProjectData <- function(object, ...) {
-  UseMethod("ProjectData")
-}
 
 #' @export
 #' @rdname ProjectData
