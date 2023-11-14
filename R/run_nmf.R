80634b46e10027684f37a707aa78c87a10073745
zdebruine <zacharydebruine@gmail.com>
2023-11-14 08:14:17 -0500

    adding IVSparse support

--- R/run_nmf.R
+++ R/run_nmf.R
@@ -32,32 +32,10 @@ run_nmf <- function(A, rank, tol = 1e-4, maxit = 100, verbose = TRUE, L1 = 0.01,
     if (!is.null(rownames(A[[1]]))) {
       if (!all(sapply(A, function(x) all.equal(rownames(x), rownames(A[[1]]))))) stop("rownames of all dgCMatrix objects in list must be identical")
     }
-    if (distributed_method == "R") {
-      # generate a distributed transpose
-      if (verbose > 0) cat("generating a distributed transpose of input matrix list\n")
-      block_sizes <- floor(c(seq(1, nrow(A[[1]]), nrow(A[[1]]) / (length(A))), nrow(A[[1]]) + 1))
-      At <- list()
-      if (verbose > 0) pb <- txtProgressBar(min = 0, max = length(A))
-      for (i in 1:length(A)) {
-        At[[i]] <- list()
-        for (j in 1:length(A)) {
-          At[[i]][[j]] <- t(A[[j]][block_sizes[i]:(block_sizes[i + 1] - 1), ])
-        }
-        At[[i]] <- do.call(rbind, At[[i]])
-        if (verbose > 0) setTxtProgressBar(pb, i)
-      }
-      if (verbose > 0) close(pb)
-      if (verbose > 0) cat("running with sparse optimization\n")
-      w_init <- matrix(stats::runif(nrow(A[[1]]) * rank), rank, nrow(A[[1]]))
-      model <- c_nmf_sparse_list(A, At, tol, maxit, verbose, L1, L2, threads, w_init)
-      rn <- rownames(A[[1]])
-      cn <- rownames(At[[1]])
-    } else {
-      w_init <- matrix(stats::runif(nrow(A[[1]]) * rank), rank, nrow(A[[1]]))
-      model <- run_nmf_on_dgCMatrix_list(A, tol, maxit, verbose, threads, w_init)
-      rn <- rownames(A[[1]])
-      cn <- do.call(c, lapply(A, colnames))
-    }
+    w_init <- matrix(stats::runif(nrow(A[[1]]) * rank), rank, nrow(A[[1]]))
+    model <- run_nmf_on_sparsematrix_list(A, tol, maxit, verbose, threads, w_init, use_vcsc)
+    rn <- rownames(A[[1]])
+    cn <- do.call(c, lapply(A, colnames))
   } else {
     if (class(A)[[1]] != "matrix") {
       if (verbose > 0) cat("running with sparse optimization\n")
