60eeeedc94564aa372d7821138bf8e8633e2c316
Skyler Ruiter <ruitersk@mail.gvsu.edu>
2024-04-17 13:12:03 -0400

    removes temp test files

--- tests/test.R
+++
@@ -1,7 +0,0 @@
-library(singlet)
-
-# Load the data
-data <- read.csv("birds.csv", header = FALSE)
-A <- as.matrix(data)
-output2 <- run_nmf(A, rank = 10, verbose = 1, method = "nnls")
-output <- run_nmf(A, rank = 10, verbose = 1, method = "scd")
